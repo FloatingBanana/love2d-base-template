e059e8cfd723e83c32c47543cad7e6c04274e247
FloatingBanana <thalesmaiademoraes13@gmail.com>
2022-06-25 11:35:47 -0300

    Entity manager callbacks emission

--- src/states/Game.lua
+++ src/states/Game.lua
@@ -7,11 +7,27 @@ function Game:enter(from, ...)
 end
 
 function Game:draw()
-    
+    EM.emit("draw")
 end
 
 function Game:update(dt)
-    
+    EM.emit("update", dt)
+end
+
+function Game:keypressed(key)
+    EM.emit("keypressed", key)
+end
+
+function Game:keyreleased(key)
+    EM.emit("keyreleased", key)
+end
+
+function Game:mousepressed(button)
+    EM.emit("mousepressed", button)
+end
+
+function Game:mousereleased(button)
+    EM.emit("mousereleased", button)
 end
 
 return Game
