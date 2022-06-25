e88b1fbb881c597d13c4a2af4ebebcc70d36d82a
FloatingBanana <thalesmaiademoraes13@gmail.com>
2022-06-25 11:35:02 -0300

    Added an input helper

---
+++ src/InputHelper.lua
@@ -0,0 +1,33 @@
+local IH = {
+    actions = {},
+    axis = {}
+}
+
+local function anyPressed(buttons)
+    for i, key in ipairs(buttons) do
+        if (lk.isDown(key)) then
+            return true
+        end
+    end
+
+    return false
+end
+
+function IH.registerAction(name, buttons)
+    IH.actions[name] = buttons
+end
+
+function IH.registerAxis(name, negative, positive)
+    IH.actions[name] = {negative = negative, positive = positive}
+end
+
+function IH.getAction(name)
+    return anyPressed(IH.actions[name])
+end
+
+function IH.getAxis(name)
+    local pos = anyPressed(IH.axis.positive) and 1 or 0
+    local neg = anyPressed(IH.axis.negative) and -1 or 0
+
+    return pos + neg
+end
