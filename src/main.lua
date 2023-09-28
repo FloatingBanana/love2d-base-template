0b2a42d49520dd4637bb33d20c31033324bfd8fa
FloatingBanana <thalesmaiademoraes13@gmail.com>
2023-09-28 14:13:43 -0300

    Added Imgui debug interface (cimgui-love library)

--- src/main.lua
+++ src/main.lua
@@ -22,6 +22,7 @@ Anim8 = require "libs.anim8"
 Draworder = require "libs.draworder"
 Concord = require "libs.concord"
 Color = require "libs.color"
+Imgui = require "libs.cimgui"
 
 Utils = require "engine.utils"
 DebugUtils = require "engine.debugUtils"
@@ -35,10 +36,12 @@ local Game = require "states.Game3d"
 local Splash = require "states.Splash2"
 
 function love.load(args)
+    Imgui.love.Init()
+
     InputHelper.registerAxis("horizontal", {"a", "left"}, {"d", "right"})
     InputHelper.registerAxis("vertical", {"w", "up"}, {"s", "down"})
 
-    GS.registerEvents({"update", "mousepressed", "mousereleased", "mousemoved", "wheelmoved", "keypressed", "keyreleased"})
+    GS.registerEvents({"update"})
     GS.switch(Game)
 end
 
@@ -48,18 +51,91 @@ function love.draw() ---@diagnostic disable-line: duplicate-set-field
 
     TransitionManager.draw()
 
+    Imgui.Render()
+    Imgui.love.RenderDrawLists()
+
     Utils.setFont(13)
     lg.setColor(1,1,1,1)
     lg.print("FPS: " .. love.timer.getFPS())
 end
 
 function love.update(dt)
+    Imgui.love.Update(dt)
+    Imgui.NewFrame()
+
     TransitionManager.update(dt)
     Timer.update(dt)
 end
+
+
 
 function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
     end
+
+    Imgui.love.KeyPressed(key)
+    if Imgui.love.GetWantCaptureKeyboard() then
+        return
+    end
+
+    GS.keypressed(key)
+end
+
+function love.keyreleased(key, ...)
+    Imgui.love.KeyReleased(key)
+    if Imgui.love.GetWantCaptureKeyboard() then
+        return
+    end
+
+    GS.keyreleased(key, ...)
+end
+
+function love.textinput(t)
+    Imgui.love.TextInput(t)
+    if Imgui.love.GetWantCaptureKeyboard() then
+        return
+    end
+
+    GS.textinput(t)
+end
+
+function love.mousemoved(x, y, ...)
+    Imgui.love.MouseMoved(x, y)
+    if Imgui.love.GetWantCaptureMouse() then
+        return
+    end
+
+    GS.mousemoved(x, y, ...)
+end
+
+function love.mousepressed(x, y, button, ...)
+    Imgui.love.MousePressed(button)
+    if Imgui.love.GetWantCaptureMouse() then
+        return
+    end
+
+    GS.mousepressed(x, y, button, ...)
+end
+
+function love.mousereleased(x, y, button, ...)
+    Imgui.love.MouseReleased(button)
+    if Imgui.love.GetWantCaptureMouse() then
+        return
+    end
+
+    GS.mousereleased(x, y, button, ...)
+end
+
+function love.wheelmoved(x, y)
+    Imgui.love.WheelMoved(x, y)
+    if Imgui.love.GetWantCaptureMouse() then
+        return
+    end
+
+    GS.wheelmoved(x, y)
+end
+
+function love.quit()
+    return Imgui.love.Shutdown()
 end
--- src/states/Game3d.lua
+++ src/states/Game3d.lua
@@ -1,19 +1,25 @@
 local Game = {}
 
+local ffi              = require "ffi"
 local Matrix           = require "engine.math.matrix"
 local Vector3          = require "engine.math.vector3"
 local Quaternion       = require "engine.math.quaternion"
 local InputHelper      = require "engine.inputHelper"
 local Model            = require "engine.3DRenderer.model"
+local Camera           = require "engine.camera3d"
+
 local PointLight       = require "engine.3DRenderer.lights.pointLight"
 local SpotLight        = require "engine.3DRenderer.lights.spotLight"
 local DirectionalLight = require "engine.3DRenderer.lights.directionalLight"
 local AmbientLight     = require "engine.3DRenderer.lights.ambientLight"
+
+local DeferredRenderer = require "engine.3DRenderer.renderers.deferredRenderer"
 local DeferredMaterial = require "engine.3DRenderer.materials.deferredPhong"
+
+local ForwardRenderer  = require "engine.3DRenderer.renderers.forwardRenderer"
 local ForwardMaterial  = require "engine.3DRenderer.materials.forwardRenderingMaterial"
 local EmissiveMaterial = require "engine.3DRenderer.materials.emissiveMaterial"
-local DeferredRenderer = require "engine.3DRenderer.renderers.deferredRenderer"
-local ForwardRenderer  = require "engine.3DRenderer.renderers.forwardRenderer"
+
 local SkyboxClass      = require "engine.3DRenderer.postProcessing.skybox"
 local SSAOClass        = require "engine.3DRenderer.postProcessing.ssao"
 local BloomClass       = require "engine.3DRenderer.postProcessing.bloom"
@@ -22,13 +28,14 @@ local ColorCorrection  = require "engine.3DRenderer.postProcessing.colorCorrecti
 local FogClass         = require "engine.3DRenderer.postProcessing.fog"
 local FXAAClass        = require "engine.3DRenderer.postProcessing.fxaa"
 local MotionBlurClass  = require "engine.3DRenderer.postProcessing.motionBlur"
-local Camera           = require "engine.camera3d"
 
-local useDeferredRendering = true
+
 
 local renderer = nil ---@type BaseRenderer
 local myModel = nil ---@type Model
 
+local lockControls = true
+local useDeferredRendering = true
 local hdrExposure = 1
 local contrast = 1
 local brightness = 0
@@ -62,7 +69,7 @@ local ambient = AmbientLight(Color(.2,.2,.2))
 local light = SpotLight(Vector3(0), Vector3(0,0,1), math.rad(17), math.rad(25.5), Color.WHITE, Color.WHITE)
 local light2 = PointLight(Vector3(0), 1, 0.005, 0.04, Color(.2,.2,.2), Color.WHITE, Color.WHITE)
 function Game:enter(from, ...)
-    lm.setRelativeMode(true)
+    lm.setRelativeMode(lockControls)
 
     local pplist = {
         skybox,
@@ -112,13 +119,13 @@ function Game:enter(from, ...)
         end
     end
 
-    renderer:addLights(ambient, light)
+    renderer:addLights(ambient, light, light2)
 end
 
 function Game:draw()
     for i, part in ipairs(drawerMesh.parts) do
         local settings = renderer:getMeshpartSettings(part)
-        settings.worldMatrix = drawerMesh.transformation * Matrix.CreateScale(Vector3(0.01)) * Matrix.CreateFromYawPitchRoll(modelRot * 20, 0, 0)
+        settings.worldMatrix = drawerMesh.transformation * Matrix.CreateScale(Vector3(0.01)) * Matrix.CreateFromYawPitchRoll(modelRot, 0, 0)
     end
 
     renderer:render(playerCam)
@@ -126,39 +133,39 @@ function Game:draw()
     if lk.isDown("q") then
         lg.draw(renderer.velocityBuffer)
     end
-
-    lg.print("HDR exposure: "..hdrExposure, 0, 30)
-    lg.print("Contrast: "..contrast, 0, 60)
-    lg.print("Brightness: "..brightness, 0, 90)
-    lg.print("Exposure: "..exposure, 0, 120)
-    lg.print("Saturation: "..saturation, 0, 150)
 end
 
 local camRot = Vector3()
 function Game:update(dt)
-    local walkdir = Vector3(
-        -InputHelper.getAxis("horizontal"),
-        0,
-        -InputHelper.getAxis("vertical")
-    )
-
-    local camRotation = Quaternion.CreateFromYawPitchRoll(camRot.yaw, camRot.pitch, camRot.roll)
-
-    if walkdir.lengthSquared > 0 then
-        playerCam.position:add(walkdir:normalize():transform(camRotation) * dt)
-    end
-
-    playerCam.position.y = playerCam.position.y + (lk.isDown("space") and 1 or lk.isDown("lshift") and -1 or 0) * dt
-    playerCam.rotation = camRotation
-
     modelRot = modelRot + dt
 
-    if lm.isDown(2) then
-        light2.position = playerCam.position:clone()
+    if lockControls then
+        local walkdir = Vector3(
+            -InputHelper.getAxis("horizontal"),
+            0,
+            -InputHelper.getAxis("vertical")
+        )
+
+        local camRotation = Quaternion.CreateFromYawPitchRoll(camRot.yaw, camRot.pitch, camRot.roll)
+
+        if walkdir.lengthSquared > 0 then
+            playerCam.position:add(walkdir:normalize():transform(camRotation) * dt)
+        end
+
+        playerCam.position.y = playerCam.position.y + (lk.isDown("space") and 1 or lk.isDown("lshift") and -1 or 0) * dt
+        playerCam.rotation = camRotation
+
+        if lm.isDown(2) then
+            light2.position = playerCam.position:clone()
+        end
+
+        if lm.isDown(1) then
+            light.position, light.direction = playerCam.position:clone(), Vector3(0,0,1):transform(camRotation)
+        end
     end
 
-    if lm.isDown(1) then
-        light.position, light.direction = playerCam.position:clone(), Vector3(0,0,1):transform(camRotation)
+    if not lockControls then
+        Game:debugGui()
     end
 end
 
@@ -168,41 +175,246 @@ function Game:mousemoved(x, y, dx, dy)
     camRot.pitch = camRot.pitch + dy * sensibility
 end
 
-function Game:wheelmoved(x, y)
-    local offset = y * (lk.isDown("tab") and 0.01 or 0.1)
-
-    if lk.isDown("1") then
-        contrast = math.max(contrast + offset, 0)
-        colorCorr:setContrast(contrast)
-    elseif lk.isDown("2") then
-        brightness = brightness + offset
-        colorCorr:setBrightness(brightness)
-    elseif lk.isDown("3") then
-        exposure = math.max(exposure + offset, 0)
-        colorCorr:setExposure(exposure)
-    elseif lk.isDown("4") then
-        saturation = math.max(saturation + offset, 0)
-        colorCorr:setSaturation(saturation)
-    else
-        hdrExposure = math.max(hdrExposure + offset, 0)
-        hdr:setExposure(hdrExposure)
+function Game:keypressed(key)
+    if key == "f1" then
+        lockControls = not lockControls
+        lm.setRelativeMode(lockControls)
     end
 end
 
-function Game:keypressed(key)
 
-end
 
-function Game:keyreleased(key)
+local names = {
+    [SkyboxClass]      = "Skybox",
+    [SSAOClass]        = "SSAO",
+    [BloomClass]       = "Bloom",
+    [HDRClass]         = "HDR",
+    [ColorCorrection]  = "Color correction",
+    [FogClass]         = "Fog",
+    [FXAAClass]        = "FXAA",
+    [MotionBlurClass]  = "Motion blur",
+    [AmbientLight]     = "Ambient light",
+    [DirectionalLight] = "Directional light",
+    [SpotLight]        = "Spot light",
+    [PointLight]       = "Point light",
+}
 
-end
+local boolPtr = ffi.new("bool[1]")
+local intPtr = ffi.new("int[1]")
+local floatPtr = ffi.new("float[3]")
+local strArrayPtr = ffi.new("const char*[3]")
 
-function Game:mousepressed(button)
+function Game:debugGui()
+    if Imgui.Begin("3D Scene", nil, Imgui.ImGuiWindowFlags_None) then
+        if Imgui.BeginTabBar("tabs", Imgui.ImGuiTabBarFlags_None) then
 
-end
+            if Imgui.BeginTabItem("General") then
+                Imgui.Text("Rendering mode: "..(useDeferredRendering and "Deferred" or "Forward"))
+                Imgui.Text(("Memory usage: %fmb"):format(collectgarbage("count") / 1024))
 
-function Game:mousereleased(button)
+                Imgui.SeparatorText("Camera")
 
+                floatPtr[0], floatPtr[1], floatPtr[2] = playerCam.position.x, playerCam.position.y, playerCam.position.z
+                if Imgui.InputFloat3("Position", floatPtr) then
+                    playerCam.position = Vector3(floatPtr[0], floatPtr[1], floatPtr[2])
+                end
+
+                floatPtr[0] = math.deg(playerCam.fov)
+                if Imgui.SliderFloat("Field of view", floatPtr, 0, 180) then
+                    playerCam.fov = math.rad(floatPtr[0])
+                end
+
+                Imgui.EndTabItem()
+            end
+
+            if Imgui.BeginTabItem("Lights") then
+                for i, light in ipairs(renderer.lights) do
+                    if Imgui.TreeNode_Str(names[getmetatable(light)]) then
+
+                        boolPtr[0] = light.enabled
+                        if Imgui.Checkbox("Enabled", boolPtr) then
+                            light.enabled = boolPtr[0]
+                        end
+
+                        if light:is(AmbientLight) then
+
+                            floatPtr[0], floatPtr[1], floatPtr[2] = light.color.r, light.color.g, light.color.b
+                            if Imgui.ColorEdit3("Ambient color", floatPtr) then
+                                light.color = Color(floatPtr[0], floatPtr[1], floatPtr[2])
+                            end
+                        else
+                            floatPtr[0], floatPtr[1], floatPtr[2] = light.position.x, light.position.y, light.position.z
+                            if Imgui.InputFloat3("Position", floatPtr) then
+                                light.position = Vector3(floatPtr[0], floatPtr[1], floatPtr[2])
+                            end
+
+                            floatPtr[0], floatPtr[1], floatPtr[2] = light.diffuse.r, light.diffuse.g, light.diffuse.b
+                            if Imgui.ColorEdit3("Diffuse color", floatPtr) then
+                                light.diffuse = Color(floatPtr[0], floatPtr[1], floatPtr[2])
+                            end
+
+                            floatPtr[0], floatPtr[1], floatPtr[2] = light.specular.r, light.specular.g, light.specular.b
+                            if Imgui.ColorEdit3("Specular color", floatPtr) then
+                                light.specular = Color(floatPtr[0], floatPtr[1], floatPtr[2])
+                            end
+
+                            floatPtr[0] = light.near
+                            if Imgui.InputFloat("Near plane", floatPtr, 0, 200) then
+                                light.near = floatPtr[0]
+                            end
+
+                            floatPtr[0] = light.far
+                            if Imgui.InputFloat("Far plane", floatPtr, 0, 200) then
+                                light.far = floatPtr[0]
+                            end
+
+                            if light:is(SpotLight) then ---@cast light SpotLight
+                                floatPtr[0] = light.innerAngle
+                                if Imgui.SliderFloat("Inner angle", floatPtr, 0, math.pi) then
+                                    light.innerAngle = floatPtr[0]
+                                end
+
+                                floatPtr[0] = light.outerAngle
+                                if Imgui.SliderFloat("Outer angle", floatPtr, 0, math.pi) then
+                                    light.outerAngle = floatPtr[0]
+                                end
+                            end
+
+                            if light:is(PointLight) then ---@cast light PointLight
+                                floatPtr[0] = light.constant
+                                if Imgui.SliderFloat("Constant", floatPtr, 0, 10) then
+                                    light.constant = floatPtr[0]
+                                end
+
+                                floatPtr[0] = light.linear
+                                if Imgui.SliderFloat("Linear", floatPtr, 0, 2) then
+                                    light.linear = floatPtr[0]
+                                end
+
+                                floatPtr[0] = light.quadratic
+                                if Imgui.SliderFloat("Quadratic", floatPtr, 0, 2) then
+                                    light.quadratic = floatPtr[0]
+                                end
+                            end
+
+                            if Imgui.TreeNode_Str("Shadow map") then
+                                if light:is(PointLight) then
+                                else
+                                    local size = Imgui.ImVec2_Float(128, 128)
+                                    Imgui.Image(light.shadowmap, size)
+                                end
+
+                                Imgui.TreePop()
+                            end
+                        end
+
+                        Imgui.TreePop()
+                    end
+                end
+
+                Imgui.EndTabItem()
+            end
+
+            if Imgui.BeginTabItem("Post processing") then
+                for i, effect in ipairs(renderer.ppeffects) do
+                    if Imgui.TreeNode_Str(names[getmetatable(effect)]) then
+
+                        if effect:is(SSAOClass) then ---@cast effect SSAO
+                            intPtr[0] = effect.algorithm == "naive" and 0 or effect.algorithm == "accurate" and 1 or 2
+                            strArrayPtr[0], strArrayPtr[1], strArrayPtr[2] = "naive", "accurate", "deferred"
+
+                            if Imgui.Combo_Str_arr("Algorithm", intPtr, strArrayPtr, 3, 3) then
+                                effect = SSAOClass(SCREENSIZE, effect.kernelSize, effect.kernelRadius, ffi.string(strArrayPtr[intPtr[0]]))
+                                renderer.ppeffects[i] = effect
+                            end
+
+                            intPtr[0] = effect.kernelSize
+                            if Imgui.SliderInt("Kernel size", intPtr, 1, 64) then
+                                effect:setKernelSize(intPtr[0])
+                            end
+
+                            floatPtr[0] = effect.kernelRadius
+                            if Imgui.SliderFloat("Kernel radius", floatPtr, 0, 2) then
+                                effect:setKernelRadius(floatPtr[0])
+                            end
+                        end
+
+                        if effect:is(HDRClass) then ---@cast effect HDR
+                            floatPtr[0] = effect.exposure
+                            if Imgui.SliderFloat("Exposure", floatPtr, 0, 5) then
+                                effect:setExposure(floatPtr[0])
+                            end
+                        end
+
+                        if effect:is(BloomClass) then ---@cast effect Bloom
+                            intPtr[0] = effect.strenght
+                            if Imgui.SliderInt("Strenght", intPtr, 0, 15) then
+                                effect.strenght = intPtr[0]
+                            end
+
+                            floatPtr[0] = effect.luminanceTreshold
+                            if Imgui.SliderFloat("Luminance treshold", floatPtr, 0, 2) then
+                                effect:setLuminanceTreshold(floatPtr[0])
+                            end
+                        end
+
+                        if effect:is(MotionBlurClass) then ---@cast effect MotionBlur
+                            floatPtr[0] = effect.amount
+                            if Imgui.SliderFloat("amount", floatPtr, 0, 1) then
+                                effect.amount = floatPtr[0]
+                            end
+                        end
+
+                        if effect:is(FogClass) then ---@cast effect Fog
+                            floatPtr[0], floatPtr[1] = effect.min, effect.max
+                            if Imgui.SliderFloat2("Treshold", floatPtr, 0, 100) then
+                                effect:setTreshold(floatPtr[0], floatPtr[1])
+                            end
+
+                            floatPtr[0], floatPtr[1], floatPtr[2] = effect.color.r, effect.color.g, effect.color.b
+                            if Imgui.ColorEdit3("Fog color", floatPtr) then
+                                effect:setColor(Color(floatPtr[0], floatPtr[1], floatPtr[2]))
+                            end
+                        end
+
+                        if effect:is(ColorCorrection) then ---@cast effect ColorCorrection
+                            floatPtr[0] = effect.brightness
+                            if Imgui.SliderFloat("Brightness", floatPtr, -1, 1) then
+                                effect:setBrightness(floatPtr[0])
+                            end
+
+                            floatPtr[0] = effect.contrast
+                            if Imgui.SliderFloat("Contrast", floatPtr, 0, 2) then
+                                effect:setContrast(floatPtr[0])
+                            end
+
+                            floatPtr[0] = effect.exposure
+                            if Imgui.SliderFloat("Exposure", floatPtr, 0, 10) then
+                                effect:setExposure(floatPtr[0])
+                            end
+
+                            floatPtr[0] = effect.saturation
+                            if Imgui.SliderFloat("Saturation", floatPtr, 0, 2) then
+                                effect:setSaturation(floatPtr[0])
+                            end
+
+                            floatPtr[0], floatPtr[1], floatPtr[2] = effect.colorFilter.r, effect.colorFilter.g, effect.colorFilter.b
+                            if Imgui.ColorEdit3("Color filter", floatPtr) then
+                                effect:setColorFilter(Color(floatPtr[0], floatPtr[1], floatPtr[2]))
+                            end
+                        end
+
+                       Imgui.TreePop()
+                    end
+                end
+
+                Imgui.EndTabItem()
+            end
+
+            Imgui.EndTabBar()
+        end
+    end
+    Imgui.End()
 end
 
 return Game
